9a1ecbf1572ddf30e0091b94a4317a69335e07bd
Elizabeth Leshuk <eleshuk@gmail.com>
2021-03-24 21:01:28 -0700

    changes to html

--- Static/js/js.js
+++ Static/js/js.js
@@ -14,7 +14,7 @@ var height = svgHeight - margin.top - margin.bottom;
 // Create an SVG wrapper, append an SVG group that will hold our chart,
 // and shift the latter by left and top margins.
 var svg = d3
-  .select(".chart")
+  .select("#scatter")
   .append("svg")
   .attr("width", svgWidth)
   .attr("height", svgHeight);
